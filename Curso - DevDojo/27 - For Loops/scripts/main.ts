a261cee199dc90d4fff67ab8cc09d48bd443c3b4
Junior Pinheiro <paladinoshot32@gmail.com>
2020-10-24 16:17:14 -0300

    forloops

---
+++ Curso - DevDojo/27 - For Loops/scripts/main.ts
@@ -0,0 +1,34 @@
+let array = ["Junior", "Pinheiro", "Ferreira"];
+//Exitem 4 tipos de For
+//primeiro tipo
+console.log("1 --------------- ");
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+console.log("2 ------------------ ");
+
+//segundo tipo
+array.forEach((name) => {
+  console.log(name);
+  //não dá pra utilizar break, continue e nem return.
+});
+
+console.log("3 ------------------ ");
+//terceiro tipo
+let person = { name: "irineu", lastname: "da silva" };
+for (let prop in person) {
+  console.log(prop); //vai retornar chave e não valor
+}
+
+for (let prop in array) {
+  console.log(prop); //vai retornar chave e não valor
+}
+
+console.log("4 ------------------ ");
+//Quarto tipo
+
+for (let prop of array) {
+  console.log(prop); //vai retornar valor
+}
