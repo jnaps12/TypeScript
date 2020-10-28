1b9f37ca13ef5e583e8f1956abb01920df676e88
Junior Pinheiro <paladinoshot32@gmail.com>
2020-10-28 11:09:31 -0300

    validando num

--- Bin2Dec/scripts/main.js
+++ Bin2Dec/scripts/main.js
@@ -1,6 +1,5 @@
 function binToDec() {
     let bin = document.getElementById("binarios").value;
-    ;
     let exponent = bin.length - 1;
     let cont = 0;
     let resultDec = 0;
@@ -14,4 +13,22 @@ function binToDec() {
 let showResult = (result) => {
     document.getElementById('result').innerHTML = String(result);
 };
+function validaNum() {
+    let numeros = document.getElementById("binarios").value;
+    let valid = true;
+    for (let cont = 0; cont < numeros.length; cont++) {
+        if (numeros[cont] != '0' && numeros[cont] != '1') {
+            valid = false;
+        }
+    }
+    let msg = document.querySelector(".msg");
+    if (!valid) {
+        document.getElementById('button').style.display = 'none';
+        msg.textContent = "* Só são permitidos 0's e 1's *";
+    }
+    else {
+        document.getElementById("button").style.display = "inline";
+        msg.textContent = '';
+    }
+}
 //# sourceMappingURL=main.js.map
--- Bin2Dec/scripts/main.ts
+++ Bin2Dec/scripts/main.ts
@@ -1,5 +1,5 @@
 function binToDec(): void {
-  let bin: string = document.getElementById("binarios").value;;
+  let bin: string = document.getElementById("binarios").value;
   let exponent: number = bin.length - 1;
   let cont: number = 0;
   let resultDec: number = 0;
@@ -17,3 +17,24 @@ let showResult = (result: number): void => {
   document.getElementById('result').innerHTML = String(result);
 }
 
+function validaNum(): void {
+  let numeros = document.getElementById("binarios").value;
+  let valid: boolean = true;
+
+  for(let cont = 0; cont < numeros.length; cont++){
+    if(numeros[cont] != '0' && numeros[cont] != '1'){
+      valid = false;
+    }
+  }
+  
+  let msg = document.querySelector(".msg");
+  if(!valid){
+    document.getElementById('button').style.display = 'none';
+    msg.textContent = "* Só são permitidos 0's e 1's *";
+  }else{
+    document.getElementById("button").style.display = "inline";
+    msg.textContent = ''
+  }
+  
+
+}
