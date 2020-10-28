48ac09c76927f78cd4ca1ff92bfa0b12c7af4f28
Junior Pinheiro <paladinoshot32@gmail.com>
2020-10-28 10:31:38 -0300

    funcional, só falta ajustes

--- Bin2Dec/scripts/main.js
+++ Bin2Dec/scripts/main.js
@@ -1,11 +1,17 @@
-let bin = "110000110101000";
-let exponent = bin.length - 1;
-let cont = 0;
-let resultDec = 0;
-while (cont < bin.length) {
-    resultDec = resultDec + Number(bin[cont]) * Math.pow(2, exponent);
-    exponent--;
-    cont++;
+function binToDec() {
+    let bin = document.getElementById("binarios").value;
+    ;
+    let exponent = bin.length - 1;
+    let cont = 0;
+    let resultDec = 0;
+    while (cont < bin.length) {
+        resultDec = resultDec + Number(bin[cont]) * Math.pow(2, exponent);
+        exponent--;
+        cont++;
+    }
+    showResult(resultDec);
 }
-console.log(resultDec);
+let showResult = (result) => {
+    document.getElementById('result').innerHTML = String(result);
+};
 //# sourceMappingURL=main.js.map
--- Bin2Dec/scripts/main.ts
+++ Bin2Dec/scripts/main.ts
@@ -1,12 +1,19 @@
-let bin: string = "110000110101000";
-let exponent: number = bin.length - 1;
-let cont: number = 0;
-let resultDec: number = 0;
+function binToDec(): void {
+  let bin: string = document.getElementById("binarios").value;;
+  let exponent: number = bin.length - 1;
+  let cont: number = 0;
+  let resultDec: number = 0;
 
-while (cont < bin.length) {
-  resultDec = resultDec + Number(bin[cont]) * Math.pow(2, exponent);
-  exponent--;
-  cont++;
+  while (cont < bin.length) {
+    resultDec = resultDec + Number(bin[cont]) * Math.pow(2, exponent);
+    exponent--;
+    cont++;
+  }
+
+  showResult(resultDec);
 }
 
-console.log(resultDec);
+let showResult = (result: number): void => {
+  document.getElementById('result').innerHTML = String(result);
+}
+
